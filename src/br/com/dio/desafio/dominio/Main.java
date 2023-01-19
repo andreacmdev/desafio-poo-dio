82c5376ab1a9b3b047902ff05de335c47d20e58e
Dedev <mmachadoacm@gmail.com>
2023-01-19 01:56:47 -0300

    testando as instancias dos objetos

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso();
+		curso1.setTitulo("java");
+		curso1.setDescricao("descricao");
+		curso1.setCargaHoraria(8);
+
+		System.out.println(curso1);
+		
+		Mentoria mentoria1 = new Mentoria();
+		mentoria1.setTitulo("gestão de projetos");
+		mentoria1.setDescricao("descricao");
+		mentoria1.setData(LocalDate.now());
+		
+		
+		System.out.println(mentoria1);
+
+	}
+
+}
